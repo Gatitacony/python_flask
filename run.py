75468704846989685d009d2e6bdaec4419d296f5
gatitacony <franciny.huaiquil.22.com>
2025-02-14 00:15:44 -0300

    run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto de Railway o 5000 por defecto
+    app.run(host="0.0.0.0", port=port, debug=True)
