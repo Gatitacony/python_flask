f4e80c61545dd1e33ff3d5121fa09c46c7bc6324
gatitacony <franciny.huaiquil.22.com>
2025-02-16 00:22:24 -0300

    migraciones, cambio de base de datos a postgress

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'keycat22')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
+    # Aquí cambias el URI de la base de datos de SQLite a PostgreSQL
+    SQLALCHEMY_DATABASE_URI = 'postgresql://franciny:keycat22@localhost/python_flask'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
