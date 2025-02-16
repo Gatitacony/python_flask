4bbaae669e2dbe3eebc0eaa4b29f7bf275fb7a5b
gatitacony <franciny.huaiquil.22.com>
2025-02-16 01:09:47 -0300

    cambiando configuracion de entorno de la base de datos para compatibilidad

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'keycat22')
     # Aquí cambias el URI de la base de datos de SQLite a PostgreSQL
-    SQLALCHEMY_DATABASE_URI = 'postgresql://franciny:keycat22@localhost/python_flask'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://franciny:keycat22@localhost/python_flask')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
